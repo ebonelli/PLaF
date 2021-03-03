4e8896b7f1f240654510257226c55c9a233d71c5
ebonelli <ebonelli@csadmins-iMac.local>
2021-03-03 13:49:16 -0500

    Typo

--- src/arith/src/ds.ml
+++ src/arith/src/ds.ml
@@ -2,19 +2,16 @@
 
 type 'a result = Ok of 'a | Error of string
 
-type 'a ea_result = env -> 'a result
-  
-let return : 'a -> 'a ea_result =
+let return : 'a -> 'a result =
   fun v ->
-  fun _env ->
   Ok v
 
-let error : string -> 'a ea_result = fun s ->
-  fun _env -> 
+let error : string -> 'a result =
+  fun s ->
   Error s
 
-let (>>=) : 'a ea_result -> ('a -> 'b ea_result) -> 'b ea_result = fun c f ->
-  fun env ->
-  match c env with
+let (>>=) : 'a result -> ('a -> 'b result) -> 'b result =
+  fun c f ->
+  match c with
   | Error err -> Error err
-  | Ok v -> f v env
+  | Ok v -> f v
